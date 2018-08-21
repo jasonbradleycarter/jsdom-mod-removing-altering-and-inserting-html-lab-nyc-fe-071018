098b13274fb237145db7a3ab50b39a947b367d81
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-21 21:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 /* Enter the code to remove the main node element under this comment */
 
-document.querySelector("main#main").remove()
+document.querySelector("main#main").remove();
 
 
-const newHeader = document.createElement("h1")
+const newHeader = document.createElement("h1");
 
-newHeader.id = "victory"
+newHeader.id = "victory";
 
-newHeader
+newHeader.innerText = "Jason is champion";
 
 /* Create your new element here and assign it to newHeader */
 
