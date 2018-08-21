6dff465652cc2adab416d65743dbef3842749c20
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-21 21:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+
 /* Enter the code to remove the main node element under this comment */
 
+document.querySelector(".main").remove()
+
+
+/*document.createElement.header = "victory"
+
+ document.body.appendChild(h1) */
+
 /* Create your new element here and assign it to newHeader */
+
 const newHeader = null;
